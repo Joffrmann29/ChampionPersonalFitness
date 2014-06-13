206b5bbf77e1cfeddcfee8b31e940218f6c9569a
Administrator <john@joffrey.att.net>
2014-06-13 19:26:24 -0400

    Initial Commit

---
+++ FitnessTracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FitnessTracker
+//
+//  Created by Administrator on 6/13/14.
+//  Copyright (c) 2014 Joffrey Mann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FitnessTracker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FitnessTracker
+//
+//  Created by Administrator on 6/13/14.
+//  Copyright (c) 2014 Joffrey Mann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
