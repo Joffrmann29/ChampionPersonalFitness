a6a0f95801079413bfeffa6cd92eae42d133085f
Joffrmann29 <Joffrmann25@gmail.com>
2014-11-19 19:51:53 -0500

    Addition to First Commit
    Addition to First Commit

---
+++ FitnessTracker/AddGoalViewController.h
@@ -0,0 +1,35 @@
+//
+//  AddGoalViewController.h
+//  FitnessTracker
+//
+//  Created by Administrator on 6/13/14.
+//  Copyright (c) 2014 Joffrey Mann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FitnessGoal.h"
+
+
+@protocol AddGoalViewControllerDelegate <NSObject>
+
+-(void)didCancel;
+-(void)didAddGoal:(FitnessGoal *)goal;
+
+@end
+
+@interface AddGoalViewController : UIViewController<UITextFieldDelegate,UITextViewDelegate,UIAlertViewDelegate>
+
+/* A delegate property which will allow us to call the protocol methods. */
+@property (weak, nonatomic) id <AddGoalViewControllerDelegate> delegate;
+
+@property (strong, nonatomic) IBOutlet UITextField *textField;
+@property (strong, nonatomic) IBOutlet UITextView *textView;
+@property (strong, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (strong, nonatomic) FitnessGoal *goal;
+@property (strong, nonatomic) IBOutlet UINavigationBar *navBar;
+
+- (IBAction)addGoal:(UIBarButtonItem *)sender;
+- (IBAction)back:(UIBarButtonItem *)sender;
+
+
+@end
---
+++ FitnessTracker/AddGoalViewController.m
@@ -0,0 +1,109 @@
+//
+//  AddGoalViewController.m
+//  FitnessTracker
+//
+//  Created by Administrator on 6/13/14.
+//  Copyright (c) 2014 Joffrey Mann. All rights reserved.
+//
+
+#import "AddGoalViewController.h"
+
+@interface AddGoalViewController ()
+
+
+@end
+
+@implementation AddGoalViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    /* set the textView and textField delegate properties to self so that we can implement their protocol methods */
+    self.textView.delegate = self;
+    self.textField.delegate = self;
+    NSLog(@"%@", self.datePicker.date);
+    [self.navBar setTitleTextAttributes:
+     [NSDictionary dictionaryWithObjectsAndKeys:
+      [UIFont fontWithName:@"Zapfino" size:17],
+      NSFontAttributeName, nil]];
+    
+    [self.navBar setBackgroundImage:[UIImage imageNamed:@"ChampionNavBar.png"] forBarMetrics:UIBarMetricsDefault];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(FitnessGoal *)returnNewGoalObject
+{
+    FitnessGoal *goalObject = [[FitnessGoal alloc] init];
+    goalObject.name = self.textField.text;
+    goalObject.desc = self.textView.text;
+    goalObject.date = self.datePicker.date;
+    goalObject.isAchieved = NO;
+    
+    return goalObject;
+}
+
+#pragma mark - UITextFieldDelegate
+
+/* Method is called when the user taps the return key. When this action occurs we tell the textField to dismiss the keyboard. */
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self.textField resignFirstResponder];
+    return YES;
+}
+
+#pragma mark - UITextViewDelegate
+
+/* UITextView Delegate method. This method is triggered when the user types a new character in the textView. */
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+    /* Test if the entered text is a return. If it is we tell textView to dismiss the keyboard and then we stop the textView from entering in additional information as text. This is not a perfect solution because users cannot enter returns in their text and if they paste text with a return items after the return will not be added. For the functionality required in this project this solution works just fine. */
+    if ([text isEqualToString:@"\n"]){
+        [self.textView resignFirstResponder];
+        return NO;
+    }
+    else return YES;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+/* If the user presses the addTaskButton call the method defined in the protocol on the delegate property. Pass in a Fitness Goal object using the method returnNewGoalObject. */
+- (IBAction)addGoal:(UIBarButtonItem *)sender
+{
+    if([self.textField.text isEqualToString:@""] || [self.textView.text isEqualToString:@""]){
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Incomplete Entry" message:@"All fields must be completed" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+        [alertView show];
+    }
+    else{
+        [self.delegate didAddGoal:[self returnNewGoalObject]];
+    }
+}
+
+- (IBAction)back:(UIBarButtonItem *)sender
+{
+    [self.delegate didCancel];
+}
+@end
